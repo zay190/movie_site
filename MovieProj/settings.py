94a4dc7b9e4a990c18ac9fc65ba8ff3fd897bc04
Okunmoyinbo Abdulzaahir <zayhq19@gmail.com>
2025-12-04 13:16:37 +0100

    prepare for live deployment

--- MovieProj/settings.py
+++ MovieProj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-rksrvs!$(@2^nm!xp)_4^!i(!fq20wq_p)^d&t*epif1zy+uyr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
